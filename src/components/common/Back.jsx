cae20c3efde04f4ebe9066fe0fa2cafd3627e20c
s375164 <jas2908@gmail.com>
2026-08-19 10:29:35 +0200

    changed Back.jsx

--- src/components/common/Back.jsx
+++ src/components/common/Back.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom';
-const Back = ({title}) => {
-    const location = useLocation();
+
+const Back = ({ title }) => {
     return (
         <>
             <section className="back">
                 <div className="overlay"></div>
                 <h1>{title}</h1>
-                <h2>Home / Pages / {location.pathname.split('/')[1]}</h2>
             </section>
             <div className="margin"></div>
         </>
