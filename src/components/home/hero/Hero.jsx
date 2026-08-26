e35e2565b00983c37800310cbf2ad908982b7757
s375164 <jas2908@gmail.com>
2026-08-26 16:25:21 +0200

    mobile ui fixed

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -25,32 +25,7 @@ const Hero = () => {
                         </div>
                     </div>
                 </div>
-            </section>
-            {/* <div className="container">
-                <div className="row">
-                    <div className="box">
-                        <i className='fa fa-car'></i>
-                        <div className="info">
-                            <h2>Easy Driving Learn</h2>
-                            <p>Clita erat ipsum lorem sit sed stet duo justo erat amet</p>
-                        </div>
-                    </div>
-                    <div className="box">
-                        <i className='fa fa-users'></i>
-                        <div className="info">
-                            <h2>National Instructor</h2>
-                            <p>Clita erat ipsum lorem sit sed stet duo justo erat amet</p>
-                        </div>
-                    </div>
-                    <div className="box">
-                        <i className='fa fa-file-alt'></i>
-                        <div className="info">
-                            <h2>Get licence</h2>
-                            <p>Clita erat ipsum lorem sit sed stet duo justo erat amet</p>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
+            </section>            
         </>
     )
 }
